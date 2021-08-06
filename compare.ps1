bc2ad1b144a4b00999737c0319bc3d7c7bb5d8ca
huaning-yang <78593320+huaning-yang@users.noreply.github.com>
2021-08-06 16:34:11 +0200

    Compare function

---
+++ compare.ps1
@@ -0,0 +1,3 @@
+﻿$file1 = Import-Csv -Path "C:\Users\yanghuan\Desktop\Test1\hashes_06-08-2021_03-26-47.csv"
+$file2 = Import-Csv -Path "C:\Users\yanghuan\Desktop\Test1\hashes_06-08-2021_03-27-06.csv"
+Compare-Object $file1 $file2 -Property Hash,Path | export-csv C:\Users\yanghuan\Desktop\Test1\output.csv
