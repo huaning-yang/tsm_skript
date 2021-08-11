05b8b5d4fcad5dc5b2da9f2c7012344e6f70afa7
huaning-yang <78593320+huaning-yang@users.noreply.github.com>
2021-08-11 16:48:31 +0200

    Delete compare.ps1

--- compare.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location -Path C:\Users\yanghuan\Desktop\Test1
-
-$hashes = @(Get-ChildItem | Sort LastWriteTime | Where-Object { $_.Name -match 'hashes' } | select FullName -Last 2)
-
-$file1 = Import-Csv $hashes[0].FullName
-$file2 = Import-Csv $hashes[1].FullName
-Compare-Object $file1 $file2 -Property Hash,Path | export-csv C:\Users\yanghuan\Desktop\Test1\output.csv
